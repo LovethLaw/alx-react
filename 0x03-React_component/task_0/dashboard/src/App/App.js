697eceb3b5e01c587858938b7785aab83d4e5121
mosescode1 <emosescode@gmail.com>
2024-08-29 16:10:39 +0100

    done

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -7,26 +7,26 @@ import Notifications from "../Notifications/Notifications";
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.handleKeydown = this.handleKeydown.bind(this);
-	}
+	// constructor(props) {
+	// 	super(props);
+	// 	this.handleKeydown = this.handleKeydown.bind(this);
+	// }
 
-	componentDidMount() {
-		window.addEventListener("keydown", this.handleKeydown)
-	}
+	// componentDidMount() {
+	// 	window.addEventListener("keydown", this.handleKeydown)
+	// }
 
-	componentWillUnmount() {
-		window.removeEventListener("keydown", this.handleKeydown)
-	}
+	// componentWillUnmount() {
+	// 	window.removeEventListener("keydown", this.handleKeydown)
+	// }
 
-	handleKeydown(event) {
-		const { logOut } = this.props;
-		if (event.ctrlKey && event.key === 'h') {
-			alert('Logging you out');
-			logOut();
-		}
-	}
+	// handleKeydown(event) {
+	// 	const { logOut } = this.props;
+	// 	if (event.ctrlKey && event.key === 'h') {
+	// 		alert('Logging you out');
+	// 		logOut();
+	// 	}
+	// }
 
 	render() {
 		return (
